98b473408057533ac6c66c97c71d6777bf34e15b
TheUpBeat <TheUpBeat@protonmail.com>
2020-06-16 02:20:00 +0530

    Script to Update the Block List

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-awk '{ if($1=="0.0.0.0") {$1= ""}; print }' $1 > hosts.txt
+sed '/^#/d' $1 > hosts_duplicate.txt
+awk '{ if($1=="0.0.0.0" || $1=="127.0.0.1") {$1= ""}; print }' hosts_duplicate.txt > hosts.txt
 sed 's/ //g' hosts.txt > hosts_duplicate.txt
 sort hosts_duplicate.txt > hosts.txt
-comm -13 <(sort Block_List.txt) <(sort hosts.txt) >> Block_List.txt
-sort Block_List.txt > hosts.txt
-sort hosts.txt > Block_List.txt
+comm -13 <(sort Facebook_Blocked.txt) <(sort hosts.txt) >> Facebook_Blocked.txt
+sort Facebook_Blocked.txt > hosts.txt
+sort hosts.txt > Facebook_Blocked.txt
 
