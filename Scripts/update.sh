98eaf03966863bb24cc85a3020b5bdb7712d967f
TheUpBeat <TheUpBeat@protonmail.com>
2020-06-16 00:41:11 +0530

    Script to Update the Block List

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 awk '{ if($1=="0.0.0.0") {$1= ""}; print }' $1 > hosts.txt
-sed 's/ //g' hosts.txt > test.txt
-sort test.txt > hosts.txt
+sed 's/ //g' hosts.txt > hosts_duplicate.txt
+sort hosts_duplicate.txt > hosts.txt
 comm -13 <(sort Block_List.txt) <(sort hosts.txt) >> Block_List.txt
 sort Block_List.txt > hosts.txt
 sort hosts.txt > Block_List.txt
